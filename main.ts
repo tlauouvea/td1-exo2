6f57d72c9823fb84a964cd8ce0c880a7543238a9
tlauouvea <61261560+tlauouvea@users.noreply.github.com>
2020-07-09 14:13:06 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+for (let index = 0; index < 2; index++) {
+    basic.showString("Bonjour")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+}
+basic.forever(function () {
+    basic.showString("Bonjour")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+})
